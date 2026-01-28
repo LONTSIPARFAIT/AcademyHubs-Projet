a02a617978e69b1847ad6c2baca54f2d38ae11db
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-01-28 16:44:43 +0100

    first commit

--- Frontend/src/types/index.ts
+++ Frontend/src/types/index.ts
@@ -1,10 +1,8 @@
-// Ré-exportation de tous les types
 export * from './course';
 export * from './user';
 export * from './auth';
 export * from './common';
 
-// Vous pouvez aussi créer des types globaux si nécessaire
 export type ApiResponse<T> = {
   success: boolean;
   data?: T;
