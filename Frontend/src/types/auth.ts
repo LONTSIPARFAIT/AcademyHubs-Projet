c6248e66c73c1b37d6a0236ef756235b577887cd
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-01-28 16:35:36 +0100

    first commit

---
+++ Frontend/src/types/auth.ts
@@ -0,0 +1,29 @@
+export interface LoginCredentials {
+  email: string;
+  password: string;
+  rememberMe?: boolean;
+}
+
+export interface RegisterCredentials {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  agreeToTerms: boolean;
+}
+
+export interface AuthResponse {
+  user: User;
+  token: string;
+  refreshToken: string;
+  expiresIn: number;
+}
+
+export interface AuthState {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  error: string | null;
+}
