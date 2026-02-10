e3f706c1ef003fe5477c08f12a96d14397a6aae0
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-02-10 10:52:35 +0100

    api axios front

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// on creer une instance d'axios pour configurer la baseURL et les headers par défaut
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api", // URL de base de votre API Laravel
   headers: {
@@ -11,11 +12,11 @@ const api = axios.create({
 // ce petit bout  de code va chercher le token dans le navigateur
 // et le donne a laravel automatiquement a chaque appel d'api
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token") // on cherche le token dans le navigateur
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}` // on le montre a laravel
     }
-    return config
+    return config // on retourne la config pour que l'appel d'api puisse continuer
 })
 
-export default api
+export default api 
