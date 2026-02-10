a1a85df793694a358f7aa28af7e6575bd27f4b64
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-02-10 06:42:24 +0100

    backend sanctum

--- Backend/app/Http/Controllers/Api/AuthController.php
+++ Backend/app/Http/Controllers/Api/AuthController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-   
+   public function register(Request $request) {
+    $request->validated([
+        "name" => "required|string",
+        "eamil" => "required|email|unique:user",
+        "password" => "required|max:6",
+    ]);
+
+    $users = User::create([
+        "name" => $request["name"],
+        "email" => $request["email"],
+        "password" => bcrypt($request['password']),
+    ]);
+   }
 }
