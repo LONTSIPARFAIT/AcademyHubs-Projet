5fae19af7781b70956171f62827d1cdfe31e4c5b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-02-10 09:19:00 +0100

    backend sanctum

--- Backend/app/Http/Controllers/Api/AuthController.php
+++ Backend/app/Http/Controllers/Api/AuthController.php
@@ -33,17 +33,18 @@ class AuthController extends Controller
 
    public function login(Request $request){
     $request->validate([
-        "email" => "required|email",
-        "password" => "required",
+        'email' => 'required|email',
+        'password' => 'required',
     ]);
 
-    $user = User::where("email", $request->email)->first();
+    $user = User::where('email', $request->email)->first();
 
-    if (!$user || !Hash::check($request->email, $user->password) ) {
+    if(!$user || !Hash::check($request->password, $user->password)) {
         return response()->json([
-            'message' => 'Invalid Credential',
-        ], 401);
+            'message' => 'Invalid credentials'
+        ], 401); // 401 : Unauthorized
     }
+
     $token = $user->createToken('auth_token')->plainTextToken;
 
     return response()->json([
