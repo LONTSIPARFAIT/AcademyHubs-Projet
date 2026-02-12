5c39cef4c2fc134b0f0690f147c7cea681f9c1c9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-02-12 14:50:28 +0100

    backend sanctum

--- Backend/app/Http/Controllers/Api/Learning/CourseController.php
+++ Backend/app/Http/Controllers/Api/Learning/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Learning;
 
 use App\Http\Controllers\Controller;
 use App\Models\Course;
+use App\Models\Lesson;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -20,25 +21,27 @@ class CourseController extends Controller
 
     public function show(Course $course) {
         // on charge les cours avec ses lecons trier selon la categorie
-        // $course->load(['category', 'instructor', 'lessons']);
-        // return response()->json($course);
-        return response()->json($course->load([
-            'category', 
-            'instructor', 
-            'sections.lessons'
-        ]));
+        $course->load(['category', 'instructor', 'lessons']);
+
+        // on ajoute l'info de l'inscription
+        $user = auth('sanctum')->user();
+
+        // on ajoute dynamiquement une propriete 'is_enrolled'
+        $course->is_enrolled = $user ? $user->enrolledCourses()->where('course_id', $course->id)->exists() : false;
+
+        return response()->json($course);
     }
 
     public function ShowLesson($slug){
         $user = auth()->user();
 
         // 1. on cherche la lesson
-        $lesson = Lesson::where('slug', $slug)->firtOrFail();
+        $lesson = Lesson::where('slug', $slug)->firstOrFail();
 
         // 2.on verifie si le user est inscrire a ce cours
         $isEnrolled = $user->enrolledCourses()->where('course_id', $lesson->course_id)->exists();
 
-        if(!$isEnroled) {
+        if(!$isEnrolled) {
             return response()->json(['message' => "Vous n'etes pas inscrire a ce cours"], 403);
         }
 
--- Backend/app/Models/User.php
+++ Backend/app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function enrolenrolledCourses(){
+    public function enrolledCourses(){
         return $this->belongsToMany(Course::class, 'enrollments') ;
     }
 }
