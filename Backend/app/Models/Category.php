0c90af14f3a79228c92b723a6c637bd7823f15e9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2026-02-27 11:30:02 +0100

    backend sanctum

--- Backend/app/Models/Category.php
+++ Backend/app/Models/Category.php
@@ -8,6 +8,6 @@ class Category extends Model
 {
     protected $fillable = ['name', 'slug', 'icon'];
     public function courses(){
-        return $this->hasMany(Course::class);
+        return $this->hasMany(Cours::class);
     }
 }
